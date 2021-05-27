435e04182c609ea9e8a95aee723cb5b9155f9bd8
Neet <navneetkaur3@gmail.com>
2021-05-26 21:01:42 -0500

    "created our enemy pizza rat"

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -1,14 +1,15 @@
+//created class hero
 class Hero{
   constructor(name){
-    this.name = name
-    this.health = 100
-    this.weapons ={
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
       sprinkleSpray: 5,
       sugarShock: 10
-    }
-    this.catchPhrases = ['i\'m fresher than day old pizza',
- 'you can\'t count my calories']
-
+    };
+    this.catchPhrases = ["i\'m fresher than day old pizza",
+ "you can\'t count my calories"];
+  }
  talkSass(){
    console.log(this.catchPhrases[0])
  }
@@ -16,7 +17,29 @@ class Hero{
    console.log(this.health)
  }
  fight(){
-   console.log('i\'m ready to rumble')
+   console.log("i\'m ready to rumble")
  }
+}
+
+//created class Enemy
+class Enemy{
+  constructor(name){
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
+      pepperoniStars: 5,
+      cheeseGrease: 10
+    };
+    this.catchPhrases = ["i\'m youtube famous",
+    "i\'m more dangerous than an uncovered sewer"];
   }
+ talkSmack(){
+   console.log(this.catchPhrases[0])
+ }
+ announceHealth(){
+   console.log(this.health)
+ }
+ fight(){
+   console.log("i\'m gonna flatten you like a slice of pepperoni!")
+ }
 }
